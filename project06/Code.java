4a61aff960117191083944479deb539ba9d721b8
rodrigovalest <rodrigo.vale.st@email.com>
2023-12-22 20:29:39 -0300

    [mod] cleaning

--- project06/Code.java
+++
@@ -1,6 +0,0 @@
-public class Code {
-    
-}
-
-// .comp() .dest() .jump()
-// receives with parameter the part of the parser
--- project06/Hi.java
+++
@@ -1,5 +0,0 @@
-class Hi {
-    public static void main(String args[]) {
-        System.out.println("Hello Java");
-    }
-}
